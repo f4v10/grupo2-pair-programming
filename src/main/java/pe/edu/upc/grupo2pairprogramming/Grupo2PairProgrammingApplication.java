82b8e8049621b47573f3a232d51cce936806e7e0
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-10-11 21:33:32 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/grupo2pairprogramming/Grupo2PairProgrammingApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.grupo2pairprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2PairProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo2PairProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/grupo2pairprogramming/Grupo2PairProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.grupo2pairprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2PairProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
